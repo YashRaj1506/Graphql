b547c2a28d5c2f8b9aa5283a44d864849867d377
YashRaj1506 <yashraj504300@gmail.com>
2025-03-26 14:06:56 +0530

    schema.py

---
+++ customer/schema.py
@@ -0,0 +1,18 @@
+import graphene
+from graphene_django import DjangoObjectType
+from . models import Client
+
+
+class ClientType(DjangoObjectType):
+    class Meta:
+        modedl = Client
+        fields = ("id", "first_name", "last_name", "city")
+
+class Query(graphene.ObjectType):
+
+    all_clients = graphene.List(ClientType)
+
+    def resolve_all_clients(self, info, **kwargs):
+        return Client.objects.all()
+    
+schema = graphene.Schema(query=Query)
